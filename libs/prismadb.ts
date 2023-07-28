a3afafb5b5ff959c0b18420a0cb23afecc9ae154
njain794 <jainn7194@gmail.com>
2023-07-28 23:14:50 +0530

    prismadb fix for dep.

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+/*import { PrismaClient } from "@prisma/client"
 
 declare global {
   var prisma: PrismaClient | undefined
@@ -7,4 +7,21 @@ declare global {
 const client = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
-export default client
+export default client*/
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
+}
+
+export default prisma;
